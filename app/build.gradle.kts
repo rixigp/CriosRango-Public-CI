1ad375945da3df0f7cb4c92c8cc115b966f14c28
Ricardo García-Prieto Villanueva <rixi18@gmail.com>
2026-09-20 00:04:28 +0200

    Bump Android version for KMP product detail migration

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -41,8 +41,8 @@ android {
         applicationId = "es.criosrango.app"
         minSdk = 24
         targetSdk = 35
-        versionCode = 119
-        versionName = "1.0.119"
+        versionCode = 120
+        versionName = "1.0.120"
     }
     signingConfigs {
         getByName("debug") {
