7da0350ee10f91532ff5e20eefc45be00226ceb6
Ricardo García-Prieto Villanueva <rixi18@gmail.com>
2026-09-20 00:17:57 +0200

    KMP: use persistent iOS WooCommerce session in smoke app

--- shared/src/iosMain/kotlin/es/criosrango/shared/MainViewController.kt
+++ shared/src/iosMain/kotlin/es/criosrango/shared/MainViewController.kt
@@ -1,3 +1,3 @@
 package es.criosrango.shared
 import androidx.compose.ui.window.ComposeUIViewController
-fun MainViewController() = ComposeUIViewController { CriosRangoSpikeScreen() }
+fun MainViewController() = ComposeUIViewController { CriosRangoSpikeScreen(IosStoreSessionStore()) }
