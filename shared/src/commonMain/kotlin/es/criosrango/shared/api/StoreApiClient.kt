efad8e276127bc9c1e972e8d57e825973f11328c
Ricardo García-Prieto Villanueva <rixi18@gmail.com>
2026-09-20 00:24:16 +0200

    KMP: use correct Ktor contentType extension for cart JSON

--- shared/src/commonMain/kotlin/es/criosrango/shared/api/StoreApiClient.kt
+++ shared/src/commonMain/kotlin/es/criosrango/shared/api/StoreApiClient.kt
@@ -13,9 +13,9 @@ import io.ktor.client.request.get
 import io.ktor.client.request.post
 import io.ktor.client.request.parameter
 import io.ktor.client.request.setBody
-import io.ktor.client.request.contentType
 import io.ktor.client.statement.bodyAsText
 import io.ktor.http.ContentType
+import io.ktor.http.contentType
 import io.ktor.http.isSuccess
 import kotlinx.coroutines.async
 import kotlinx.coroutines.awaitAll
