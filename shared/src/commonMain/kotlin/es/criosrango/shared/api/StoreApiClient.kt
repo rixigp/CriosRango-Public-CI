c89afe4438a490edcaf5c1f02e3d2495944af427
Ricardo García-Prieto Villanueva <rixi18@gmail.com>
2026-09-20 00:20:50 +0200

    KMP: fix shared cart request content-type import

--- shared/src/commonMain/kotlin/es/criosrango/shared/api/StoreApiClient.kt
+++ shared/src/commonMain/kotlin/es/criosrango/shared/api/StoreApiClient.kt
@@ -13,6 +13,7 @@ import io.ktor.client.request.get
 import io.ktor.client.request.post
 import io.ktor.client.request.parameter
 import io.ktor.client.request.setBody
+import io.ktor.client.request.contentType
 import io.ktor.client.statement.bodyAsText
 import io.ktor.http.ContentType
 import io.ktor.http.isSuccess
