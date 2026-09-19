4b22d71161b1064a8976f9a061e642b3c089f7e2
Ricardo García-Prieto Villanueva <rixi18@gmail.com>
2026-09-19 20:58:14 +0200

    Export ktor-client-core from shared API

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -18,7 +18,7 @@ kotlin {
             implementation(compose.foundation)
             implementation(compose.material3)
             implementation(compose.ui)
-            implementation("io.ktor:ktor-client-core:2.3.12")
+            api("io.ktor:ktor-client-core:2.3.12")
             implementation("io.ktor:ktor-client-content-negotiation:2.3.12")
             implementation("io.ktor:ktor-serialization-kotlinx-json:2.3.12")
             implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.7.3")
