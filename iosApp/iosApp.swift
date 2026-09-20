d2861a6f25ed4d53747f750170a5696b1f970bc1
Ricardo García-Prieto Villanueva <rixi18@gmail.com>
2026-09-20 10:36:31 +0200

    Fase H: forward iOS Cecabank callbacks to shared

--- iosApp/iosApp.swift
+++ iosApp/iosApp.swift
@@ -9,10 +9,22 @@ final class CriosRangoIOSApp: UIResponder, UIApplicationDelegate {
         _ application: UIApplication,
         didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil
     ) -> Bool {
+        MainViewControllerKt.VerifyIosPendingPayment()
         let window = UIWindow(frame: UIScreen.main.bounds)
         window.rootViewController = MainViewControllerKt.MainViewController()
         self.window = window
         window.makeKeyAndVisible()
+        return true
+    }
+
+    func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:]) -> Bool {
+        MainViewControllerKt.HandleIosPaymentCallback(rawUrl: url.absoluteString)
+        return true
+    }
+
+    func application(_ application: UIApplication, continue userActivity: NSUserActivity, restorationHandler: @escaping ([UIUserActivityRestoring]?) -> Void) -> Bool {
+        guard userActivity.activityType == NSUserActivityTypeBrowsingWeb, let url = userActivity.webpageURL else { return false }
+        MainViewControllerKt.HandleIosPaymentCallback(rawUrl: url.absoluteString)
         return true
     }
 }
