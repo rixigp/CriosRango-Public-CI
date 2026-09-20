1d37084a48933f6ed6d821876524c05a4f10d1b5
Ricardo García-Prieto Villanueva <rixi18@gmail.com>
2026-09-20 08:45:58 +0200

    Import Ktor request header for checkout app marker

--- shared/src/commonMain/kotlin/es/criosrango/shared/api/StoreApiClient.kt
+++ shared/src/commonMain/kotlin/es/criosrango/shared/api/StoreApiClient.kt
@@ -15,6 +15,7 @@ import es.criosrango.shared.model.StoreProduct
 import io.ktor.client.HttpClient
 import io.ktor.client.call.body
 import io.ktor.client.request.get
+import io.ktor.client.request.header
 import io.ktor.client.request.post
 import io.ktor.client.request.parameter
 import io.ktor.client.request.setBody
