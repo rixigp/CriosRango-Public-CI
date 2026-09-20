33f4f516fdea9d02d3caacc01db2d735755749fc
Ricardo García-Prieto Villanueva <rixi18@gmail.com>
2026-09-20 11:10:25 +0200

    Fase H: route universal link callback through shared runtime

--- iosApp/iosApp.swift
+++ iosApp/iosApp.swift
@@ -24,7 +24,7 @@ final class CriosRangoIOSApp: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, continue userActivity: NSUserActivity, restorationHandler: @escaping ([UIUserActivityRestoring]?) -> Void) -> Bool {
         guard userActivity.activityType == NSUserActivityTypeBrowsingWeb, let url = userActivity.webpageURL else { return false }
-        MainViewControllerKt.HandleIosPaymentCallback(rawUrl: url.absoluteString)
+        IosPaymentRuntimeKt.handleIosPaymentCallback(rawUrl: url.absoluteString)
         return true
     }
 }
