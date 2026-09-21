8da3e716ea7b22ada502ea5a94355fc57e1ec5f3
Ricardo García-Prieto Villanueva <rixi18@gmail.com>
2026-09-21 12:38:41 +0200

    I.4: fix shared account repository import

--- shared/src/iosMain/kotlin/es/criosrango/shared/MainViewController.kt
+++ shared/src/iosMain/kotlin/es/criosrango/shared/MainViewController.kt
@@ -1,6 +1,7 @@
 package es.criosrango.shared
 
 import androidx.compose.ui.window.ComposeUIViewController
+import es.criosrango.shared.account.AccountRepository
 
 fun MainViewController() = ComposeUIViewController {
     CriosRangoIOSAccountScreen(AccountRepository(IosAccountTokenStore()))
