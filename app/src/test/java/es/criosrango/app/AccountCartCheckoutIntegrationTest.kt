14183befe6742816ea9c4f962d2c0b784e34ec2e
Ricardo García-Prieto Villanueva <rixi18@gmail.com>
2026-09-21 09:32:41 +0200

    I.3: remove unused test import

--- app/src/test/java/es/criosrango/app/AccountCartCheckoutIntegrationTest.kt
+++ app/src/test/java/es/criosrango/app/AccountCartCheckoutIntegrationTest.kt
@@ -2,7 +2,6 @@ package es.criosrango.app
 
 import org.junit.Assert.assertEquals
 import org.junit.Assert.assertFalse
-import org.junit.Assert.assertNull
 import org.junit.Assert.assertTrue
 import org.junit.Test
 
