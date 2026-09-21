6e139d5a6204abb28e74a515e15a0505e8aae174
Ricardo García-Prieto Villanueva <rixi18@gmail.com>
2026-09-21 12:33:37 +0200

    I.4: fix shared account screen ColumnScope import

--- shared/src/commonMain/kotlin/es/criosrango/shared/IosAccountScreen.kt
+++ shared/src/commonMain/kotlin/es/criosrango/shared/IosAccountScreen.kt
@@ -2,6 +2,7 @@ package es.criosrango.shared
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.ColumnScope
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
