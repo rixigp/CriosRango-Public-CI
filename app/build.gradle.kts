94b7ee6c0066b0b16916ace21f064fa89b93ae32
Ricardo García-Prieto Villanueva <rixi18@gmail.com>
2026-09-22 12:10:26 +0200

    J.5 bump Android version to 1.0.139 and remove Retrofit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -41,8 +41,8 @@ android {
         applicationId = "es.criosrango.app"
         minSdk = 24
         targetSdk = 35
-        versionCode = 138
-        versionName = "1.0.138"
+        versionCode = 139
+        versionName = "1.0.139"
     }
     signingConfigs {
         getByName("debug") {
@@ -76,8 +76,6 @@ dependencies {
     implementation("androidx.lifecycle:lifecycle-runtime-compose:2.8.7")
     implementation("androidx.navigation:navigation-compose:2.8.5")
     implementation("io.coil-kt:coil-compose:2.7.0")
-    implementation("com.squareup.retrofit2:retrofit:2.11.0")
-    implementation("com.squareup.retrofit2:converter-gson:2.11.0")
     implementation("com.squareup.okhttp3:logging-interceptor:4.12.0")
     implementation("com.google.code.gson:gson:2.11.0")
     implementation(platform("com.google.firebase:firebase-bom:34.19.0"))
