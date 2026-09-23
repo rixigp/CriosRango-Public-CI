588b9061db3b5ddc22c2743bde64d16f2ee357e8
Ricardo García-Prieto Villanueva <rixi18@gmail.com>
2026-09-23 11:49:20 +0200

    K.2 shared cart store

---
+++ shared/src/commonTest/kotlin/es/criosrango/shared/StoreCartStoreTest.kt
@@ -0,0 +1,27 @@
+package es.criosrango.shared
+
+import es.criosrango.shared.api.StoreSessionStore
+import kotlin.test.Test
+import kotlin.test.assertEquals
+import kotlin.test.assertNull
+
+class StoreCartStoreTest {
+    @Test
+    fun sessionStoreKeepsCartCredentialsAcrossReads() {
+        val session = InMemoryStoreSessionStore()
+        session.cartToken = "cart-token"
+        session.nonce = "nonce"
+        session.cookieHeader = "wordpress_logged_in=x"
+        assertEquals("cart-token", session.cartToken)
+        assertEquals("nonce", session.nonce)
+        assertEquals("wordpress_logged_in=x", session.cookieHeader)
+    }
+
+    @Test
+    fun emptySessionStartsWithoutCartCredentials() {
+        val session: StoreSessionStore = InMemoryStoreSessionStore()
+        assertNull(session.cartToken)
+        assertNull(session.nonce)
+        assertNull(session.cookieHeader)
+    }
+}
